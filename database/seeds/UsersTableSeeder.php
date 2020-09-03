241a04abbfabf35087e5c3f3ec5e211bb3f4a108
Esteban Bocardo <toti_irre@hotmail.com>
2020-09-02 20:13:47 -0500

    Commit inicial, se monto la plantilla default y modulo basico de auth

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "Luis García Rodriguez Alias el Tocayo",
+            'email' => "correo@correo.com",
+            'password' => Hash::make('1234'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Esteban Bocardo Medel",
+            'email' => "esteban@gmail.com",
+            'password' => Hash::make('asdasd'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "Kevin Arnold Sahagun",
+            'email' => "Kevin@correo.com",
+            'password' => Hash::make('1234'),
+        ]);
+    }
+}
